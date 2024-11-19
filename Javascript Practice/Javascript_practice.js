6bc59cac35d31dd4d5630e8f35f2ab918a1ebcac
Prakhar <prakhar2788@gmail.com>
2024-11-19 17:16:40 +0530

    [Prakhar] Add: Methods used in array for Iteration in an array or getting a new array out of the existing one

--- Javascript Practice/Javascript_practice.js
+++ Javascript Practice/Javascript_practice.js
@@ -1,20 +1,15 @@
 console.log("Array Practice");
 
-1. Adding/Removing Elements
+//2. Iterating Over Arrays
 
-//push: Adds one or more elements to the end of an array.
+//forEach(): Executes a function for each array element.
+console.log("forEach()");
 let arr = [1, 2, 3];
-arr.push(4); // [1, 2, 3, 4]
+arr.forEach(num => console.log(num)); // 1, 2, 3
 
-//pop: Removes the last element from an array and returns it.
-let arr = [1, 2, 3];
-let last = arr.pop(); // last = 3, arr = [1, 2]
-
-//unshift(): Adds one or more elements to the start of an array.
-let arr = [2, 3];
-arr.unshift(1); // [1, 2, 3]
-
-//shift(): Removes the first element from an array and returns it.
-let arr = [1, 2, 3];
-let first = arr.shift(); // first = 1, arr = [2, 3]
-
+//map(): Creates a new array by applying a function to each element.
+console.log("map()");
+let arr1 = [1, 2, 3];
+let squaredarr = arr1.map(num => num * num); // [1, 4, 9]
+console.log("old array",arr1); 
+console.log("new squared array",squaredarr); 
