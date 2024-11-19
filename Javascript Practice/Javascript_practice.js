7109b409cd1a9a26329c7671b13aaf092df332d6
Prakhar <prakhar2788@gmail.com>
2024-11-19 18:24:12 +0530

    [Prakhar] Add: Methods used in array for Combining arrays and Checking presence of some element in an array

--- Javascript Practice/Javascript_practice.js
+++ Javascript Practice/Javascript_practice.js
@@ -1,18 +1,17 @@
 console.log("Array Practice");
 
-//6. Copying and Extracting
+//7. Combining and Checking
 
-//slice(): Returns a shallow copy of a portion of the array.
-console.log("slice()");
-let arr = [1, 2, 3, 4];
-let subArray = arr.slice(1, 3); // [2, 3]
-console.log("original array",arr);
-console.log("Shallow copy ",subArray); 
+//concat(): Combines two or more arrays.
+console.log("concat()");
+let arr1 = [1, 2];
+let arr2 = [3, 4];
+let combined = arr1.concat(arr2); // [1, 2, 3, 4]
+console.log("combined array ",combined); 
 
-//splice(): Adds/removes elements from a specific index.
-console.log("splice()");
-let arr1 = [1, 2, 3];
-console.log("original array",arr1);
-arr1.splice(1, 1, 99); // [1, 99, 3]
-// (1,1,99): index=1, how many elements to remove=1, which new element to insert=99
-console.log("spliced array",arr1); 
+//includes(): Checks if an element is present in the array.
+console.log("includes()");
+let arr = [1, 2, 3];
+console.log(arr);
+let has2 = arr.includes(2); // true
+console.log("does arr contain 2?",has2); 
