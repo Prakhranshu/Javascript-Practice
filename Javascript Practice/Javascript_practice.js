196326e3c72c41e398e15d6a3107ad48d0a51c42
Prakhar <prakhar2788@gmail.com>
2024-11-19 18:39:05 +0530

    [Prakhar] Add: Reduce method to reduce an array to a single value by applying a function

--- Javascript Practice/Javascript_practice.js
+++ Javascript Practice/Javascript_practice.js
@@ -1,17 +1,9 @@
 console.log("Array Practice");
 
-//7. Combining and Checking
+//8. Reducing
 
-//concat(): Combines two or more arrays.
-console.log("concat()");
-let arr1 = [1, 2];
-let arr2 = [3, 4];
-let combined = arr1.concat(arr2); // [1, 2, 3, 4]
-console.log("combined array ",combined); 
-
-//includes(): Checks if an element is present in the array.
-console.log("includes()");
-let arr = [1, 2, 3];
-console.log(arr);
-let has2 = arr.includes(2); // true
-console.log("does arr contain 2?",has2); 
+//reduce(): Reduces the array to a single value by applying a function.
+console.log("reduce()");
+let arr = [1, 2, 3, 4];
+let sum = arr.reduce((acc, num) => acc + num, 0); // 10
+console.log("sum of array ",sum); 
